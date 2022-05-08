0c2d1e562446bbcca76650681574a5dccc2d34cc
Rataysh <kiril.ebarev@gmail.com>
2022-05-08 22:04:58 +0300

    taskCodeWars

---
+++ task.sol
@@ -0,0 +1,13 @@
+pragma solidity ^0.4.19;
+
+contract CountByX {
+
+  function countBy(int x, int n) view returns (int[]) {
+    int[] z;
+    // TODO: Your code here
+    for (int i = 1; i <= n; i++) {
+        z.push(x*i);
+    }      
+    return z;
+  }
+}
