10d1d6c5aafa91f69aa7f79314faff8c1b2d91a1
Rataysh <kiril.ebarev@gmail.com>
2022-05-10 14:25:11 +0300

    summul

--- tasks(all_easy).sol
+++ tasks(all_easy).sol
@@ -126,3 +126,24 @@ contract Kata {
     } else {
         return year / 100 + 1;
 }}}
+
+
+// Task 8
+// Summation
+// Write a program that finds the summation of every number from 1 to num. 
+// The number will always be a positive integer greater than 0.
+// For example:
+// summation(2) -> 3
+// 1 + 2
+// summation(8) -> 36
+// 1 + 2 + 3 + 4 + 5 + 6 + 7 + 8
+
+contract Kata {
+  function summation(int n) public view returns (int) {
+    int result = 0;
+    for (int i = 1; i < n + 1; i++) {
+        result += i;
+    }
+    return result;
+  }
+}
