6ff47622f40c115c8a9522d1e72bd69588625110
Rataysh <kiril.ebarev@gmail.com>
2022-05-10 12:23:13 +0300

    Task 7

--- tasks(all_easy).sol
+++ tasks(all_easy).sol
@@ -107,3 +107,22 @@ contract CountByX {
   }
 }
 
+
+
+// Task 7
+// The first century spans from the year 1 up to and including the year 100, the second century - 
+// from the year 101 up to and including the year 200, etc.
+// Given a year, return the century it is in.
+// Examples
+// 1705 --> 18
+// 1900 --> 19
+// 1601 --> 17
+// 2000 --> 20
+
+contract Kata {
+  function century(int year) public pure returns (int) {
+    if (year / 100 == year / 100 + year % 100) {
+        return year / 100;
+    } else {
+        return year / 100 + 1;
+}}}
